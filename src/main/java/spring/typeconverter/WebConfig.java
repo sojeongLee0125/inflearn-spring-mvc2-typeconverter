21efd78675a4edf91faf6c855114c07911c7ce5d
sojeonglee <sojeonglee0125@gmail.com>
2022-11-30 21:17:30 +0900

    feat : 포맷터를 웹 어플리케이션에 적용

--- src/main/java/spring/typeconverter/WebConfig.java
+++ src/main/java/spring/typeconverter/WebConfig.java
@@ -3,19 +3,24 @@ package spring.typeconverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import spring.typeconverter.converter.IntegerToStringConverter;
 import spring.typeconverter.converter.IpPortToStringConverter;
-import spring.typeconverter.converter.StringToIntegerConverter;
 import spring.typeconverter.converter.StringToIpPortConverter;
+import spring.typeconverter.formatter.MyNumberFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+
+        // 주석 처리 - 컨버터가 포맷터보다 우선순위가 높디/
+        // registry.addConverter(new StringToIntegerConverter());
+        // registry.addConverter(new IntegerToStringConverter());
+
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 포맷터 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
